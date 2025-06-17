4a1702f21a3f382e2bcc75b24a82a78d34079c93
Andrew Bykovsky <a.bykovsky2517@gmail.com>
2025-06-17 16:52:38 +0300

    Init plugin

---
+++ mu-tc-link-manager.php
@@ -0,0 +1,12 @@
+<?php  
+
+/*
+Plugin Name: TC Link Manager
+Description: Plugin sends info about links statuses to TG - bot
+Version: 1.0
+Author: Herobet | Andrew B.
+*/
+
+require_once( __DIR__ . '/tc_link_manager/tc-link-manager.php' );
+
+?>
